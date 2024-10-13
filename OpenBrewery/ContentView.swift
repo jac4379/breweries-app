96ac10fb55f0cf3a2c6b56285dab2799e5754d48
jac4379 <jwelch8514@gmail.com>
2024-10-13 17:32:16 -0500

    Initial Commit

---
+++ OpenBrewery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenBrewery
+//
+//  Created by Jacqueline on 10/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenBrewery/OpenBreweryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenBreweryApp.swift
+//  OpenBrewery
+//
+//  Created by Jacqueline on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenBreweryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
